082e4abba2bd59e2d0eea6e9d9605c42992aebe8
Chiozzi <mchiozzi95@gmail.com>
2021-12-01 18:52:31 -0300

    change case style

--- Core/Inc/magnetic_field.h
+++ Core/Inc/magnetic_field.h
@@ -12,15 +12,15 @@
 #define PI_4 4 * M_PI
 
 typedef struct {
-	float x_intensity;
-	float y_intensity;
-	float z_intensity;
+	double x;
+	double y;
+	double z;
 } MagneticField;
 
 typedef struct {
-	float x_position;
-	float y_position;
-	float z_position;
+	float x;
+	float y;
+	float z;
 } SpacePosition;
 
 typedef struct {
@@ -29,24 +29,25 @@ typedef struct {
 	float z;
 } DistanceVector;
 
-
 typedef struct {
 	int id;
-	float frequency;
-	double magnetic_moment;
-	double magnetic_cte;
-	SpacePosition position;
-	MagneticField magneticFieldIntensity;
+	float f_frequency;
+	double d_magnetic_moment;
+	double d_magnetic_cte;
+	SpacePosition sp_position;
+	MagneticField mf_intensity;
 } MagneticFieldSource;
 
 MagneticField sampleMagneticField(
 		HAL_StatusTypeDef (*readMagneticSensor)(I2C_HandleTypeDef*, float*),
 		I2C_HandleTypeDef *i2c);
 
-MagneticFieldSource* identifyMagneticField(MagneticField *mfSamples);
+void identifyMagneticField(double *d_mf_x_samples, double *d_mf_y_samples,
+		double *d_mf_z_samples, MagneticFieldSource *mf_nodes,
+		uint16_t ui16_sample_size);
 
 double getDistanceFromRSS(MagneticFieldSource node);
 
-SpacePosition estimatePoisition(MagneticFieldSource *nodes, int qnty_nodes);
+SpacePosition estimatePoisition(MagneticFieldSource *nodes);
 
 #endif
--- Core/Src/magnetic_field.c
+++ Core/Src/magnetic_field.c
@@ -1,49 +1,66 @@
 #include "magnetic_field.h"
+#include "dft.h"
 
 MagneticField sampleMagneticField(
 		HAL_StatusTypeDef (*readMagneticSensor)(I2C_HandleTypeDef*, float*),
 		I2C_HandleTypeDef *i2c) {
-	MagneticField mfSample;
+	MagneticField mf_sample;
 
-	float response[3];
+	float f_response[3];
 
-	if (HAL_OK == readMagneticSensor(i2c, response)) {
+	if (HAL_OK == readMagneticSensor(i2c, f_response)) {
 
-		mfSample.x_intensity = response[0];
-		mfSample.y_intensity = response[1];
-		mfSample.z_intensity = response[2];
+		mf_sample.x = f_response[0];
+		mf_sample.y = f_response[1];
+		mf_sample.z = f_response[2];
 	}
 
-	return mfSample;
+	return mf_sample;
 }
 
-MagneticFieldSource* identifyMagneticField(MagneticField *mfSamples) {
-	MagneticFieldSource mfSources[3];
+void identifyMagneticField(double *d_mf_x_samples, double *d_mf_y_samples, double *d_mf_z_samples,
+		MagneticFieldSource *mf_nodes, uint16_t ui16_sample_size) {
+
+	double inimag[ui16_sample_size];
+
+	double f_mf_intensity_z_real_out[ui16_sample_size];
+	double f_mf_intensity_z_img_out[ui16_sample_size];
+
+
+	compute_dft_real_pair(d_mf_z_samples, inimag, f_mf_intensity_z_real_out,
+			f_mf_intensity_z_img_out, ui16_sample_size);
+
+	f_mf_intensity_z_real_out;
+	f_mf_intensity_z_img_out;
 
 	// perform dft
 
 	// identify fields and intensity
-
-	return mfSources;
 }
 
 double getDistanceFromRSS(MagneticFieldSource node) {
-	double distance = cbrt(
-			node.magnetic_cte / node.magneticFieldIntensity.z_intensity);
+	double distance = cbrt(node.d_magnetic_cte / node.mf_intensity.z);
 
 	return distance;
 }
 
-SpacePosition estimatePoisition(MagneticFieldSource *nodes, int qnty_nodes) {
-	SpacePosition spRXPosition;
+SpacePosition estimatePoisition(MagneticFieldSource *nodes ) {
+	SpacePosition sp_rx_position;
 
-	double fDistances[3];
+	double d_distances[3];
 
-	for (int node_index = 0; node_index < qnty_nodes; node_index++) {
-		fDistances[node_index] = getDistanceFromRSS(nodes[node_index]);
-
+	for (int nodeIndex = 0; nodeIndex < 3; nodeIndex++) {
+		d_distances[nodeIndex] = getDistanceFromRSS(nodes[nodeIndex]);
 	}
 
-	return spRXPosition;
+	sp_rx_position.x = (pow(d_distances[0], 2) - pow(d_distances[1], 2)
+			+ pow(nodes[1].sp_position.x, 2)) / (2 * nodes[1].sp_position.x);
+	sp_rx_position.y = (pow(d_distances[0], 2) - pow(d_distances[2], 2)
+			+ pow(nodes[2].sp_position.x, 2) + pow(nodes[2].sp_position.y, 2)
+			- 2 * nodes[2].sp_position.x * sp_rx_position.x)
+			/ (2 * nodes[2].sp_position.y);
+	sp_rx_position.z = 0;
+
+	return sp_rx_position;
 }
 
